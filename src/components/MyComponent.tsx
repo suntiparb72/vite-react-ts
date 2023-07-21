ea955eea53aebd4c65c368545aa67c3081465e77
Suntiparb <pae@Suntiparbs-MacBook-Air.local>
2023-07-21 09:06:58 +0700

    init: setup-project

---
+++ src/components/MyComponent.tsx
@@ -0,0 +1,15 @@
+function MyComponent() {
+  return (
+    <div>
+      <p>
+        Lorem tester dolor sit amet consectetur adipisicing elit. Minima,
+        assumenda asperiores quis iste ducimus maxime id ipsam labore eligendi
+        fugiat eum, animi atque sed autem corrupti modi molestias illum.
+        Laboriosam?
+      </p>
+      <p>This is my Component</p>
+    </div>
+  );
+}
+
+export default MyComponent;
